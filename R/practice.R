bebc77d7c36ee5c62c52728222776d02b70a17b2
Berenice Gonzalez <bjgonzalez@arizona.edu>
2023-04-25 09:32:06 -0700

    Made a larger map, nearly all the rows are included with the exception of one.

--- R/practice.R
+++ R/practice.R
@@ -9,17 +9,14 @@ View(newdataframe)
 newdataframe$Latitude <- as.numeric(newdataframe$Latitude)
 newdataframe$Longitude <- as.numeric(newdataframe$Longitude)
 
-
+# Larger map, spanning from Mexico to Peru 
+mymap <- get_map(location = c(-120, -20, -90, 30), source = "stamen", maptype = "toner", crop = FALSE, zoom = 4)
+ggmap(mymap) + 
+  geom_point(aes(x = Longitude, y = Latitude), data = newdataframe, alpha = .5, color="darkred", size = 3)  
 
 #these specific coordinates belong to Peru (-81.356, -18.348, -68.673, -0.057)
-mymap <- get_map(location = c(-81.356, -18.348, -68.673, -0.057), source = "stamen", maptype = "toner", crop = FALSE)
-ggmap(mymap) + 
-  geom_point(aes(x = Longitude, y = Latitude), data = newdataframe, alpha = .5, color="darkred", size = 3)
+#mymap <- get_map(location = c(-81.356, -18.348, -68.673, -0.057), source = "stamen", maptype = "toner", crop = FALSE)
+#ggmap(mymap) + 
+  #geom_point(aes(x = Longitude, y = Latitude), data = newdataframe, alpha = .5, color="darkred", size = 3) 
 
-
-#separate the coordinate data from the rest of the data in newdataframe
-#tom.coords.df <- data.frame(Latitude = newdataframe$Latitude, Longitude = newdataframe$Longitude, Location = newdataframe$Collecting_locality)
-
-#lat <- tom.coords.df$Latitude
-#lon <- tom.coords.df$Longitude
  
