a03b94888e0def6a0ff6597cf90c89d58da1df0a
Berenice Gonzalez <bjgonzalez@arizona.edu>
2023-04-27 12:38:38 -0700

    Made map larger, added a title and the axix names. Made geom_point values for species and country.

--- R/practice.R
+++ R/practice.R
@@ -1,22 +1,33 @@
 library(ggmap)
-practicetableread <- read.delim(file = 'accessions.tsv')
-View(practicetableread)
+accessionstable <- read.delim(file = 'accessions.tsv')
+View(accessionstable)
 
-
-newdataframe <- practicetableread[practicetableread$Latitude != '' & practicetableread$Latitude != 'unknown',]
+newaccessions <- accessionstable[accessionstable$Latitude != '' & accessionstable$Latitude != 'unknown',]
 View(newdataframe)
 
-newdataframe$Latitude <- as.numeric(newdataframe$Latitude)
-newdataframe$Longitude <- as.numeric(newdataframe$Longitude)
+newaccessions$Latitude <- as.numeric(newaccessions$Latitude)
+newaccessions$Longitude <- as.numeric(newaccessions$Longitude)
+
 
 # Larger map, spanning from Mexico to Peru 
-mymap <- get_map(location = c(-120, -20, -90, 30), source = "stamen", maptype = "toner", crop = FALSE, zoom = 4)
-ggmap(mymap) + 
-  geom_point(aes(x = Longitude, y = Latitude), data = newdataframe, alpha = .5, color="darkred", size = 3)  
+tomato.map <- get_map(location = c(-120, -20, -90, 30), source = "stamen", maptype = "toner", crop = FALSE, zoom = 4)
+ggmap(tomato.map) + 
+  geom_point(aes(x = Longitude, y = Latitude, color = species), data = newaccessions, alpha = .5, size = 4) +
+  scale_color_manual(values = c(lycopersicum = "#cc444b", pimpinellifolium = "#55a630")) + 
+  labs(title = "Tomato Accessions", x = "Longitude", y = "Latitude")
+
+
+#creates a table with any missing values that were left out of the previous line of code 
+missing_value <- newaccessions[is.na(newaccessions$Longitude) | is.na(newaccessions$Latitude), ]
+
+
+#color points based on country, adjust color = Country in geom_point 
+    #scale_color_manual(values = c(NIC = "#907ad6", MEX = "#cc444b", PER = "#219ebc", ECU = "#02c39a", CRI = "#ffafcc", HND = "#fb8500", COL = "#ffc300", SLV = "#003f88", GTM = "#55a630"))
 
 #these specific coordinates belong to Peru (-81.356, -18.348, -68.673, -0.057)
-#mymap <- get_map(location = c(-81.356, -18.348, -68.673, -0.057), source = "stamen", maptype = "toner", crop = FALSE)
-#ggmap(mymap) + 
-  #geom_point(aes(x = Longitude, y = Latitude), data = newdataframe, alpha = .5, color="darkred", size = 3) 
+   #tomato.map <- get_map(location = c(-81.356, -18.348, -68.673, -0.057), source = "stamen", maptype = "toner", crop = FALSE)
+       #ggmap(tomato.map) + 
+       #geom_point(aes(x = Longitude, y = Latitude), data = newaccessions, alpha = .5, color="darkred", size = 3) 
+
 
  
